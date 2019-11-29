7545db0bd55317ccc39ffb43c4778265625d25e8
fahmiazhar24 <fahmiazhar24@gmail.com>
2019-11-29 20:04:59 +0700

    admin menu and form input

---
+++ raporkuAPP/formInputAdmin.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace raporkuAPP
+{
+    partial class formInputAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namaWali = new System.Windows.Forms.TextBox();
+            this.userWali = new System.Windows.Forms.TextBox();
+            this.passWali = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // namaWali
+            // 
+            this.namaWali.Location = new System.Drawing.Point(253, 160);
+            this.namaWali.Name = "namaWali";
+            this.namaWali.Size = new System.Drawing.Size(121, 22);
+            this.namaWali.TabIndex = 0;
+            // 
+            // userWali
+            // 
+            this.userWali.Location = new System.Drawing.Point(253, 201);
+            this.userWali.Name = "userWali";
+            this.userWali.Size = new System.Drawing.Size(121, 22);
+            this.userWali.TabIndex = 1;
+            // 
+            // passWali
+            // 
+            this.passWali.Location = new System.Drawing.Point(253, 251);
+            this.passWali.Name = "passWali";
+            this.passWali.Size = new System.Drawing.Size(121, 22);
+            this.passWali.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "IPA",
+            "IPS"});
+            this.comboBox1.Location = new System.Drawing.Point(253, 297);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nama Lengkap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kelas";
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(133, 362);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 8;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(133, 442);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // formInputAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.passWali);
+            this.Controls.Add(this.userWali);
+            this.Controls.Add(this.namaWali);
+            this.Name = "formInputAdmin";
+            this.Text = "formInputAdmin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox namaWali;
+        private System.Windows.Forms.TextBox userWali;
+        private System.Windows.Forms.TextBox passWali;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ raporkuAPP/menuAdmin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace raporkuAPP
+{
+    public partial class menuAdmin : Form
+    {
+        public menuAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void btInputWali_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btInputSiswa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
